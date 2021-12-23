0b7f95d7bc6610ba2c0acaeb6a2c2b5613183025
teguhap <61332266+teguhap@users.noreply.github.com>
2021-12-24 02:27:47 +0700

    Initial commit

---
+++ app/src/main/java/com/project/cashhere/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.project.cashhere
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Handler().postDelayed({ Intent(this,Dashboard :: class.java).also {
+            startActivity(it)
+        } },3000L)
+    }
+
+}
