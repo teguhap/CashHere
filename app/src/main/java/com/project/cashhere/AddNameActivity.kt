c1f8b9a4931a5288bb69339c1757f922ef7f32e1
teguhap <61332266+teguhap@users.noreply.github.com>
2021-12-24 06:28:12 +0700

    Add Activity DashboardActivity.kt

--- app/src/main/java/com/project/cashhere/AddNameActivity.kt
+++ app/src/main/java/com/project/cashhere/AddNameActivity.kt
@@ -1,7 +1,9 @@
 package com.project.cashhere
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import androidx.core.content.ContextCompat
 
 class AddNameActivity : AppCompatActivity() {
@@ -10,5 +12,12 @@ class AddNameActivity : AppCompatActivity() {
         setContentView(R.layout.activity_add_name)
 
         window.statusBarColor = ContextCompat.getColor(this,R.color.black)
+        val btnSimpan = findViewById<Button>(R.id.btnAddName)
+
+        btnSimpan.setOnClickListener {
+            Intent(this,DashboardActivity :: class.java).also {
+                startActivity(it)
+            }
+        }
     }
 }
--- app/src/main/java/com/project/cashhere/DashboardActivity.kt
+++ app/src/main/java/com/project/cashhere/DashboardActivity.kt
@@ -2,10 +2,15 @@ package com.project.cashhere
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.content.ContextCompat
 
 class DashboardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+        window.decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR)
+        window.statusBarColor = ContextCompat.getColor(this,R.color.white)
+
     }
 }
